8a07331d398f521744bae2705b61cbc378ccfa56
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-25 16:31:19 +0800

    add form styles

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -2,21 +2,33 @@ import styled from 'styled-components';
 import SwapSlippage from './SwapSlippage';
 import TokenBalance from './TokenBalance';
 import TokenInput from './TokenInput';
+import TradeDetails from './TradeDetails';
 
 const TradeForm = () => {
     return (
-        <Form>
-            <SwapSlippage />
-            <TokenBalance />
-            <TokenInput />
-            <TokenBalance />
-            <TokenInput />
-            <SwapButton>Swap</SwapButton>
-        </Form>
+        <Container>
+            <Form>
+                <SwapSlippage />
+                <TokenBalance />
+                <TokenInput />
+                <TokenBalance />
+                <TokenInput />
+                <SwapButton>Swap</SwapButton>
+                <TradeDetails />
+            </Form>
+        </Container>
     );
 };
 
-const Form = styled.form``;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+const Form = styled.form`
+    border: 1px solid black;
+    
+    padding: 10px;
+`;
 const SwapButton = styled.button``;
 
 export default TradeForm;
