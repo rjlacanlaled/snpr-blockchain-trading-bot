8e35649f98b77e6f143c24627a66447295c7a2d1
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-25 17:40:56 +0800

    added modal styled component

---
+++ src/components/styles/Modal.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Modal = styled.div`
+    display: ${({ show }) => (show ? 'flex' : 'none')};
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    z-index: 10;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(0, 0, 0, 0.8);
+`;
