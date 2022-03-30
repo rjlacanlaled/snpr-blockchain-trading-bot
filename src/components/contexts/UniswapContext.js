12d9cfaf670465a93c0a14635207fb43bd130563
rjlacanlaled <rjlacanlaled@gmail.com>
2022-03-30 18:50:50 +0800

    update uniswap context

--- src/components/contexts/UniswapContext.js
+++ src/components/contexts/UniswapContext.js
@@ -1,312 +1,51 @@
-import { createContext, useEffect, useState } from 'react';
-import useBlockchainNetwork from '../hooks/useBlockchain';
+import { createContext } from 'react';
+import useBlockchain from '../hooks/useBlockchain';
 import pancakeswapAbi from '../../data/pancakeswap-router-v2-abi.json';
-import uniswapAbi from '../..//data/uniswap-router-v3-abi.json';
-import { ChainId, Token, WETH, Fetcher, Trade, Route, TokenAmount, TradeType, Percent } from '@pancakeswap-libs/sdk-v2';
+import usePersist from '../hooks/usePersist';
 import { ethers } from 'ethers';
-import useFloat from '../hooks/useFloat';
-import { update } from 'eth/core';
+import { ChainId, Token, WETH, Fetcher, Trade, Route, TokenAmount, TradeType, Percent } from '@pancakeswap-libs/sdk-v2';
 
 export const UniswapContext = createContext();
 
 const defaultRouter = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
+const defaultGasPrice = ethers.utils.parseUnits('5', 'gwei');
+const defaultGas = ethers.utils.parseUnits('500000', 'wei');
 
-const UniswapContextProvider = ({ children }) => {
-    const { network, account, getTokenBalance, getTokenDecimal, eth } = useBlockchainNetwork();
-    const [router, setRouter] = useState();
+const UniswapContextProvider = ({ children, provider }) => {
+    const { getContract, provider } = useBlockchain;
+    const [router, setRouter] = usePersist(defaultRouter);
 
-    useEffect(() => {
-        updateRouter();
-    }, []);
+    const getUniswapContract = ({ from, gas = defaultGas, gasPrice = defaultGasPrice }) => {
+        if (!wallet) return;
 
-    useEffect(() => {
-        setRouter(new network.web3.eth.Contract(pancakeswapAbi, defaultRouter, { from: account }));
-    }, [account, network]);
-
-    const updateRouter = () => {
-        const contract = new network.web3.eth.Contract(pancakeswapAbi, defaultRouter, {
-            from: process.env.REACT_APP_WALLET_ADDRESS,
-            gasPrice: JSON.parse(localStorage.getItem('gasFee')),
-            gas: JSON.parse(localStorage.getItem('gasLimit')),
-        });
-
-        setRouter(contract);
+        return getContract(pancakeswapAbi, router, { from, gas, gasPrice });
     };
 
-    const swapExactTokensForTokens = async (amountIn, tokenIn, tokenOut, slippage, gasPrice, gasLimit, deadline) => {
-        const tx = await router.methods
-            .swapExactTokensForTokens(0.001, 12, 'dsadasdsadas', 'dsadasdas', 'dasdsadsad')
-            .estimateGas();
-    };
-
-    const swapEthForExactTokens = async () => {};
-
-    const sendTransaction = async (amountIn, tokenIn, tokenOut, slippage, gasPrice, gasLimit, deadline) => {
-        let parsedGas, parsedGasPrice;
-
-        console.log(gasPrice);
-
-        try {
-            parsedGas = ethers.utils.parseUnits(gasLimit.toString(), 'wei');
-            parsedGasPrice = ethers.utils.parseUnits('5', 'gwei');
-        } catch (ex) {
-            console.log(ex.toString());
-        }
-
-        const transactionParameters = {
-            nonce: '0x00', // ignored by MetaMask
-            gasPrice: '8000', // customizable by user during MetaMask confirmation.
-            gas: '5', // customizable by user during MetaMask confirmation.
-            to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
-            from: account, // must match user's active address.
-            value: '0x00', // Only required to send ether to the recipient from the initiating external account.
-            data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
-            chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
-        };
-
-        // txHash is a hex string
-        // As with any RPC call, it may throw an error
-        const txHash = await eth.request({
-            method: 'eth_sendTransaction',
-            params: [transactionParameters],
-        });
-    };
-
-    const swapExactTokensForTokensSupportingFeeOnTransferTokens = async (
-        amountIn,
-        tokenIn,
-        tokenOut,
-        slippage,
-        tokenInDec,
-        tokenOutDec
-    ) => {
-
-        console.log({tokenInDec, tokenOutDec})
-        const tokenInDecimal = tokenInDec || await getTokenDecimal(tokenIn);
-        const tokenOutDecimal = tokenOutDec || await getTokenDecimal(tokenOut);
-
-        console.log({ amountIn, tokenIn, tokenOut, slippage, tokenInDec, tokenOutDec });
-
-        const parsedAmountIn = ethers.utils.parseUnits(amountIn, tokenInDecimal);
-
-        const tradeDetails = await getTradeDetails(
-            parsedAmountIn,
-            tokenIn,
-            tokenInDecimal,
-            tokenOut,
-            tokenOutDecimal,
-            parseFloat(slippage)
-        );
-
-        if (tradeDetails[0] < 1) return false;
-
-        const { trade, slippageTolerance } = tradeDetails[1];
-
-        console.log(trade);
-        const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw.toString();
-
-        console.log(amountOutMin);
-
-        try {
-            const tx = await router.methods.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-                parsedAmountIn,
-                amountOutMin,
-                [tokenIn, tokenOut],
-                process.env.REACT_APP_WALLET_ADDRESS,
-                parseFloat('1000000') * 60000
-            );
-
-     
-
-            const gas = await tx.estimateGas();
-
-           
-
-            const transactionRouter = getTransactionRouter();
-
-            const parsedGasPrice = ethers.utils.parseUnits(JSON.parse(localStorage.getItem('gasFee')), 'gwei');
-            const parsedGasLimit = ethers.utils.parseUnits(
-                (gas + parseFloat(JSON.parse(localStorage.getItem('gasLimit')))).toString(),
-                'wei'
-            );
-
-            
-            const executeTx = await transactionRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-                parsedAmountIn,
-                amountOutMin,
-                [tokenIn, tokenOut],
-                process.env.REACT_APP_WALLET_ADDRESS,
-                Math.floor(Date.now() / 1000) + 60 * 20,
-                {
-                    gasLimit: parsedGasLimit,
-                    gasPrice: parsedGasPrice,
-                }
-            );
-
-            console.log(executeTx);
-
-            return [1, executeTx];
-        } catch (err) {
-            console.log(err);
-            return [-1, err.toString()];
-        }
-    };
-
-    const swapExactTokensForTokensSupportingFeeOnTransferWithAmountOutMin = async (
-        amountIn,
-        tokenIn,
-        tokenOut,
-        slippage,
-        expectedAmountOutMin,
-        tokenInDec,
-        tokenOutDec
-    ) => {
-        try {
-            const tokenInDecimal = tokenInDec || (await getTokenDecimal(tokenIn));
-            const tokenOutDecimal = tokenOutDec || (await getTokenDecimal(tokenOut));
-
-            const tradeDetails = await getTradeDetails(
-                amountIn,
-                tokenIn,
-                tokenInDecimal,
-                tokenOut,
-                tokenOutDecimal,
-                parseFloat(slippage)
-            );
-
-            if (tradeDetails[0] < 1) return false;
-
-            const { trade, slippageTolerance } = tradeDetails[1];
-            const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw.toString();
-
-            console.log({ amountOutMin: ethers.utils.formatUnits(amountOutMin, tokenOutDecimal) });
-            console.log({
-                expectedAmountOutMin: ethers.utils.formatUnits(expectedAmountOutMin.toString(), tokenOutDecimal),
-            });
-
-            try {
-                if (!ethers.BigNumber.from(amountOutMin).gte(ethers.BigNumber.from(expectedAmountOutMin)))
-                    return [-1, amountOutMin];
-            } catch (err) {
-                return [-1, err.troStrin()];
-            }
-
-            try {
-                updateRouter();
-                const tx = await router.methods.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-                    amountIn,
-                    amountOutMin,
-                    [tokenIn, tokenOut],
-                    process.env.REACT_APP_WALLET_ADDRESS,
-                    parseFloat('1000000') * 60000
-                );
-
-                const gas = await tx.estimateGas();
-
-                const transactionRouter = getTransactionRouter();
-
-                const parsedGasPrice = ethers.utils.parseUnits(JSON.parse(localStorage.getItem('gasFee')), 'gwei');
-                const parsedGasLimit = ethers.utils.parseUnits(
-                    (gas + parseFloat(JSON.parse(localStorage.getItem('gasLimit')))).toString(),
-                    'wei'
-                );
-
-                const executeTx = await transactionRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-                    amountIn,
-                    amountOutMin,
-                    [tokenIn, tokenOut],
-                    process.env.REACT_APP_WALLET_ADDRESS,
-                    Math.floor(Date.now() / 1000) + 60 * 20,
-                    {
-                        gasLimit: parsedGasLimit,
-                        gasPrice: parsedGasPrice,
-                    }
-                );
-
-                return [1, executeTx];
-            } catch (err) {
-                return [-1, err.toString()];
-            }
-        } catch (err) {
-            return [-1, err.toString()];
-        }
-    };
-
-    const getTransactionRouter = () => {
-        const routerContract = new ethers.Contract(
-            defaultRouter,
-            pancakeswapAbi,
-            network.wallet.connect(network.provider)
-        );
-
-        return routerContract;
-    };
-
-    const getDefaultFromToken = () => {
-        const defaultFromToken = JSON.parse(localStorage.getItem('defaultBuyToken'));
-        if (defaultFromToken == 'undefined') return '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-        return defaultFromToken;
-    };
-
-    const getDefaultToToken = () => {
-        const defaultToToken = JSON.parse(localStorage.getItem('defaultSellToken'));
-        if (defaultToToken == 'undefined') return '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
-        return defaultToToken;
-    };
-
-    const getFormmatedTradeInputs = async (amountIn, tokenIn, tokenOut) => {
-        console.log('amountIn', amountIn);
-        const tokenInDecimal = await getTokenDecimal(tokenIn);
-        const tokenOutDecimal = await getTokenDecimal(tokenOut);
-        const parsedAmountIn = ethers.utils.parseUnits(amountIn, tokenInDecimal);
-
-        return { tokenInDecimal, tokenOutDecimal, parsedAmountIn };
-    };
-
-    const getTradeDetails = async (amountIn, tokenIn, tokenInDecimal, tokenOut, tokenOutDecimal, slippage) => {
+    const getAmountOutMin = async (amountIn, tokenIn, tokenInDecimal, tokenOut, tokenOutDecimal, slippage) => {
         try {
             const slippageTolerance = new Percent(Math.floor(slippage * 100).toString(), '10000');
-
             const tokenA = new Token(ChainId.MAINNET, ethers.utils.getAddress(tokenIn), tokenInDecimal);
-
             const tokenB = new Token(ChainId.MAINNET, ethers.utils.getAddress(tokenOut), tokenOutDecimal);
-
             const pair = await Fetcher.fetchPairData(tokenA, tokenB, network.wallet.connect(network.provider));
             const route = new Route([pair], tokenA);
             const trade = new Trade(route, new TokenAmount(tokenA, amountIn), TradeType.EXACT_INPUT);
-            const amountOutMin = ethers.utils.formatUnits(
-                trade.minimumAmountOut(slippageTolerance).raw.toString(),
-                tokenOutDecimal
-            );
+            const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw.toString();
+            const formattedAmountOutMin = ethers.formatUnits(amountOutMin, tokenOutDecimal);
 
-            console.log('========IN HERE=========');
-
-            return [1, { trade, slippageTolerance, amountOutMin }];
+            return { amountOutMin, formattedAmountOutMin};
         } catch (ex) {
             console.log(ex.toString());
-            return [-1, ex.toString()];
         }
+
+        return false;
     };
 
-    return (
-        <UniswapContext.Provider
-            value={{
-                router,
-                setRouter,
-                getDefaultFromToken,
-                getDefaultToToken,
-                swapExactTokensForTokens,
-                swapExactTokensForTokensSupportingFeeOnTransferTokens,
-                swapExactTokensForTokensSupportingFeeOnTransferWithAmountOutMin,
-                sendTransaction,
-                updateRouter,
-                getFormmatedTradeInputs,
-                getTradeDetails,
-            }}
-        >
-            {children}
-        </UniswapContext.Provider>
-    );
+    const uniswap = {
+        getUniswapContract,
+        getAmountOutMin,
+    };
+
+    return <UniswapContext.Provider value={{ uniswap }}>{children}</UniswapContext.Provider>;
 };
 
 export default UniswapContextProvider;
